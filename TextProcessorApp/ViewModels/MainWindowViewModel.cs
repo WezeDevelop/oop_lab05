0bc9ce7d561f86a3b14fe867a376ff0f9e882e77
WezeDevelop <103280050+WezeDevelop@users.noreply.github.com>
2025-05-19 07:07:19 +0300

    Add files via upload

---
+++ TextProcessorApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Text;
+
+namespace TextProcessorApp.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string inputText = string.Empty;
+
+    [ObservableProperty]
+    private string outputText = string.Empty;
+
+    [RelayCommand]
+    private void Process()
+    {
+        // Видаляємо всі цифри
+        var noDigits = new StringBuilder();
+        foreach (var c in InputText)
+        {
+            if (!char.IsDigit(c))
+                noDigits.Append(c);
+        }
+
+        // Подвоюємо всі одинарні пробіли
+        OutputText = noDigits.ToString().Replace(" ", "  ");
+    }
+}
---
+++ TextProcessorApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TextProcessorApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
